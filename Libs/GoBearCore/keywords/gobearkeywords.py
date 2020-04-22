764a8da8b50acb3bef37b2456e8ad8e37a3ea240
Nyon Nguyen <nyonnguyen@gmail.com>
2020-04-22 23:52:57 +0700

    set date month

--- Libs/GoBearCore/keywords/gobearkeywords.py
+++ Libs/GoBearCore/keywords/gobearkeywords.py
@@ -135,31 +135,32 @@ class GoBearCoreKeywords(LibraryComponent):
 
         # get date picker
         date_picker = self.get_date_picker_popup()
-        # get date picker values
-        datepicker_switch_value = self.get_date_picker_switch_values(date_picker)
-        current_month = datepicker_switch_value['month'] or None
-        current_year = datepicker_switch_value['year'] or None
 
         # check if date picker is Days mode
         if self.is_date_picker_days():
             _position = 0
+            # get date picker values
+            datepicker_switch_value = self.get_date_picker_switch_values()
+            current_month = datepicker_switch_value['month'] or None
+            current_year = datepicker_switch_value['year'] or None
+
             if current_year != year:
-                self.select_datepicker_year_at(date_picker, None, None, year, _position)
+                self.select_datepicker_year_at(date_picker, day, month, year, _position)
             elif current_month != month:
-                self.select_datepicker_month_at(date_picker, None, month, None, _position)
+                self.select_datepicker_month_at(date_picker, day, month, year, _position)
             else:
                 # current_month == month and current_year == year
-                self.select_datepicker_day_at(date_picker, day, None, None, _position)
+                self.select_datepicker_day_at(date_picker, day, month, year, _position)
 
         if self.is_date_picker_months():
             _position = 1
             # get month popup
-            current_date_picker = date_picker.get_element_by_class(GB_DATE_PICKER_MONTHS_CLASS)
+            # current_date_picker = date_picker.get_element_by_class(GB_DATE_PICKER_MONTHS_CLASS)
 
         if self.is_date_picker_years():
             _position = 2
             # get years popup
-            current_date_picker = date_picker.get_element_by_class(GB_DATE_PICKER_YEARS_CLASS)
+            # current_date_picker = date_picker.get_element_by_class(GB_DATE_PICKER_YEARS_CLASS)
 
     def select_datepicker_year_at(self, datepicker, day, month, year, position=2):
         """
@@ -176,6 +177,8 @@ class GoBearCoreKeywords(LibraryComponent):
             # at Month => click once on switch
             self.click_date_picker_switch(1)
         self.select_datepicker_value(datepicker, GB_DATE_PICKER_YEARS_CLASS, year)
+        self.select_datepicker_value(datepicker, GB_DATE_PICKER_MONTHS_CLASS, month)
+        self.select_datepicker_value(datepicker, GB_DATE_PICKER_DAYS_CLASS, day)
 
     def select_datepicker_month_at(self, date_picker, day, month, year, position=1):
         if position == 0:
@@ -185,6 +188,7 @@ class GoBearCoreKeywords(LibraryComponent):
             # at Year => select Year
             self.select_datepicker_value(year)
         self.select_datepicker_value(date_picker, GB_DATE_PICKER_MONTHS_CLASS, month)
+        self.select_datepicker_value(date_picker, GB_DATE_PICKER_DAYS_CLASS, day)
 
     def select_datepicker_day_at(self, datepicker, day, month, year, position=0):
         if position == 1:
@@ -197,8 +201,11 @@ class GoBearCoreKeywords(LibraryComponent):
         self.select_datepicker_value(datepicker, GB_DATE_PICKER_MONTHS_CLASS, day)
 
     def select_datepicker_value(self, datepicker, picker_class, value):
-        current_date_picker = datepicker.find_ealement_by_class(picker_class)
-        values = current_date_picker.find_elements_by_xpath("//tbody//td")
+        current_date_picker = datepicker.get_element_by_class(picker_class)
+        if picker_class == GB_DATE_PICKER_DAYS_CLASS:
+            values = current_date_picker.find_elements_by_xpath("//tbody//td")
+        else:
+            values = current_date_picker.find_elements_by_xpath("//tbody//td/span")
         available_values = [v for v in values if not self.elementKeys.is_element_contain_class('disabled', v)]
         for v in available_values:
             if v.get_textContent() == value:
@@ -213,9 +220,16 @@ class GoBearCoreKeywords(LibraryComponent):
             time.sleep(0.25)
 
     def get_date_picker_switch(self):
-        return self.elementKeys.find_element_by_class(GB_DATE_PICKER_SWITCH_CLASS)
+        if self.is_date_picker_days():
+            picker_class = GB_DATE_PICKER_DAYS_CLASS
+        elif self.is_date_picker_months():
+            picker_class = GB_DATE_PICKER_MONTHS_CLASS
+        else:
+            picker_class = GB_DATE_PICKER_YEARS_CLASS
+        current_picker = self.elementKeys.find_element_by_class(GB_DATE_PICKER_MONTHS_CLASS)
+        return current_picker.get_element_by_class(GB_DATE_PICKER_SWITCH_CLASS)
 
-    def get_date_picker_switch_values(self, datepicker):
+    def get_date_picker_switch_values(self):
         datepicker_switch = self.get_date_picker_switch()
         _raw = datepicker_switch.get_textContent()
         if ' ' in _raw:
